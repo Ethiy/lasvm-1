52aa29195e9eda6a5b1278ec837ed4b158cc4987
Ethiy <oussama.ennafii@polytechnique.org>
2016-11-03 23:44:52 +0100

    second sketch for the intance template

--- src/Instance/instance.h
+++ src/Instance/instance.h
@@ -3,38 +3,70 @@
 
 #include <iostream>
 #include <map>
+#include <utility>
+#include <algorithm>
 
 namespace lasvm
 {
     /*!
-    * Instance class
+    * Instance<REAL> class
     */
     template <typename REAL>
     class Instance
     {
     public:
         Instance( void);
-        Instance( std::istream&);
+        Instance( std::istream &);
+        Instance( const Instance<REAL> &);
+        Instance( const Instance<REAL> &&);
         ~Instance( void);
-
-        friend Instance operator+(const left_instance &, const right_instance &);
-        friend Instance operator-(const left_instance &, const right_instance &);
-        friend Instance operator*(const left_instance &, const right_instance &);
-
-        size_t zero_norm();
-        REAL absolute_value_norm();
-        REAL euclidian_norm();
-        REAL uniform_norm();
+        
+        Instance& operator=( const Instance<REAL> &);
+        Instance& operator+=( const Instance<REAL> &);
+        Instance& operator-=( const Instance<REAL> &);
+        Instance& operator*=( const Instance<REAL> &);
 
     private:
-        std::map<size_t> vector;
-        size_t size;
+        std::map<size_t,REAL> vector;
+        size_t length;
 
-        friend std::ostream & operator<<( std::ostream &, const Instance &);
+        friend std::ostream & operator<<( std::ostream &, const Instance<REAL> &);
+    };
+
+    template<REAL>
+    inline Instance<REAL>::Instance( void):length(0) {}
+    template<REAL>
+    inline Instance<REAL>::Instance( const Instance<REAL> &X): length(X.length), vector(X.vector){}
+    template<REAL>
+    inline Instance<REAL>::Instance( const Instance<REAL> &&X ) noexcept : length( std::move(X.length)), vector( std::move(X.vector)) {}
+    template<REAL>
+    inline Instance<REAL>::~Instance( void){}
+    
+    template<REAL>
+    inline Instance<REAL> & operator=( const Instance<REAL> & X)
+    {
+        std::swap( length, X.length);
+        std::swap( vector, X.vector);
+        return *this;
     }
 
-    inline Instance::Instance( void):size(0) {}
-    inline Instance::~Instance( void){}
+    template<REAL>
+    inline Instance<REAL> operator+=( Instance<REAL> lhs, const Instance<REAL> & rhs)
+    {
+        return lhs;
+    }
+
+    template<REAL>
+    inline Instance<REAL> operator-=( Instance<REAL> lhs, const Instance<REAL> & rhs)
+    {
+        return lhs;
+    }
+
+    template<REAL>
+    inline Instance<REAL> operator*=( Instance<REAL> lhs, const Instance<REAL> & rhs)
+    {
+        return lhs;
+    }
 
 }
 
