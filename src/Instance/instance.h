f75ea99ded61140fb2ce0e9aab9308b5156472fd
Ethiy <oussama.ennafii@polytechnique.org>
2016-11-04 01:00:29 +0100

    third sketch

--- src/Instance/instance.h
+++ src/Instance/instance.h
@@ -20,11 +20,34 @@ namespace lasvm
         Instance( const Instance<REAL> &);
         Instance( const Instance<REAL> &&);
         ~Instance( void);
+
+        void swap( const Instance<REAL> & X) throw()
+        {
+            std::swap( length, X.length);
+            std::swap( vector, X.vector);
+        }
         
-        Instance& operator=( const Instance<REAL> &);
-        Instance& operator+=( const Instance<REAL> &);
-        Instance& operator-=( const Instance<REAL> &);
-        Instance& operator*=( const Instance<REAL> &);
+        Instance& operator=( const Instance<REAL> &)
+        {
+            X.swap(*this);
+            return *this;
+        }
+        REAL& operator[](size_t index) {return vector[index];}
+        REAL& 
+
+        Instance& operator+=( const Instance<REAL> &)
+        {
+            length = std::max( length, X.length);
+            std::transform( std::begin(X.vector), std::(X.vector), std::begin(vector), [ &vector, X.vector](std::pair<size_t,REAL> p)
+                                                                                        {
+                                                                                            //-----------------------------------------------------
+                                                                                            return p;
+                                                                                        }
+                          );
+            return *this;
+        }
+        Instance& operator-=( const Instance<REAL> &);//-----------------------------------------------------------------------------------
+        Instance& operator*=( const Instance<REAL> &);//-----------------------------------------------------------------------------------
 
     private:
         std::map<size_t,REAL> vector;
@@ -43,31 +66,38 @@ namespace lasvm
     inline Instance<REAL>::~Instance( void){}
     
     template<REAL>
-    inline Instance<REAL> & operator=( const Instance<REAL> & X)
-    {
-        std::swap( length, X.length);
-        std::swap( vector, X.vector);
-        return *this;
-    }
+    inline void swap( Instance<REAL>& X, Instance<REAL>& Y) { X.swap(Y);}
 
     template<REAL>
-    inline Instance<REAL> operator+=( Instance<REAL> lhs, const Instance<REAL> & rhs)
+    inline Instance<REAL> operator+( Instance<REAL> lhs, const Instance<REAL> & rhs)
     {
+        lhs += rhs;
         return lhs;
     }
 
     template<REAL>
-    inline Instance<REAL> operator-=( Instance<REAL> lhs, const Instance<REAL> & rhs)
+    inline Instance<REAL> operator-( Instance<REAL> lhs, const Instance<REAL> & rhs)
     {
+        lhs -= rhs;
         return lhs;
     }
 
     template<REAL>
-    inline Instance<REAL> operator*=( Instance<REAL> lhs, const Instance<REAL> & rhs)
+    inline Instance<REAL> operator*( Instance<REAL> lhs, const Instance<REAL> & rhs)
     {
+        lhs *= rhs;
         return lhs;
     }
 
+    template<REAL>
+    inline bool operator==( Instance<REAL> lhs, const Instance<REAL> & rhs)
+    {
+        //------------------------------------------------------------------------------------------------
+        return false;
+    }
+
+    template<REAL>
+    inline bool operator!=( Instance<REAL>lhs, const Instance<REAL> & rhs) {return !operator==(lhs,rhs);}
 }
 
 #endif // !INSTANCE_H
