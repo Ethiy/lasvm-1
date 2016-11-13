115ef3c7aad87f74141fbfb0442bd648f10161f1
ETHIY <ETHIY@BullWolf.local>
2016-11-13 19:30:55 +0100

    fifth sketch

--- src/Instance/instance.cpp
+++ src/Instance/instance.cpp
@@ -4,94 +4,161 @@
 namespace lasvm
 {
     // ------------ Constructors: void, std::string, copy and move --------------------------------------
-    template<real_t> Instance<real_t>::Instance( void):length(0) {}
-    template<real_t> Instance<real_t>::Instance( const Instance<real_t> &other): 
+    template<typename real_t> Instance<real_t>::Instance( void):length(0) {}
+    template<typename real_t> Instance<real_t>::Instance( const Instance<real_t> &other): 
                                                             length(other.length), vector(other.vector){}
-    template<real_t> Instance<real_t>::Instance( const Instance<real_t> &&other ) noexcept :
-                                        length( std::move(other.length)), vector( std::move(other.vector)) {}
-    template<real_t> Instance<real_t>::~Instance( void){}
+    template<typename real_t> Instance<real_t>::Instance( Instance<real_t> && other ) noexcept
+    {
+        length = std::move( other.length);
+        vector = std::move( other.vector);
+    }
+    template<typename real_t> Instance<real_t>::~Instance( void){}
 
 
     // ------------------- Copy-Swap implementation ----------------------------------------------------
+    template<typename real_t>
     void Instance<real_t>::swap( const Instance<real_t> & other) throw()
     {
         std::swap( length, other.length);
         std::swap( vector, other.vector);
     }
 
+    template<typename real_t>
     Instance<real_t>& Instance<real_t>::operator=( Instance<real_t> other)
     {
         other.swap(*this);
         return *this;
     }
-    template<real_t> void swap( Instance<real_t>& X, Instance<real_t>& Y)
+    template<typename real_t>
+    void swap( Instance<real_t>& lhs, Instance<real_t>& rhs)
     {
         using std::swap;
-        X.swap(Y);
+        lhs.swap(rhs);
+    }
+
+    template<typename real_t>
+    Instance<real_t>& Instance<real_t>::operator=( Instance<real_t> && other) noexcept
+    {
+        length = std::exchange( other.length, 0);
+        vector = std::exchange( other.vector, std::map<size_t, real_t>());
+        return *this;
     }
 
     //------------------------ Operators: [], +=, -=, *= ------------------------------------------------
-    real_t& operator[](size_t index)
+    template<typename real_t> 
+    real_t& Instance<real_t>::operator[](size_t index)
     {
-        
-        if()
-            return ;
+        auto it = vector.find(index);
+        if(it != vector.end())
+            return vector[index];
         else
             return 0;
     }
-    Instance<real_t>& Instance<real_t>::operator+=( const Instance<real_t> & other);
+    template<typename real_t>
+    Instance<real_t>& Instance<real_t>::operator+=( const Instance<real_t> & other)
     {
         length = std::max( length, other.length);
-        std::for_each( std::begin(other.vector), std::(other.vector), [ &vector](std::pair<size_t,real_t> p)
-                                                                        {
-                                                                            //-----------------------------------------------------
-                                                                        }
-                      );
+        std::for_each( std::begin(other.vector), std::end(other.vector), [&vector](std::pair<size_t,real_t> p)
+                                                                            {
+                                                                                auto iterator = vector.find( p.first);
+                                                                                if( iterator != m.end())
+                                                                                    vector[p.first] += p.second;
+                                                                                else
+                                                                                    vector[p.first] = p.second;
+                                                                            }
+                                                                        );
         return *this;
     }
+    template<typename real_t>
     Instance<real_t>& Instance<real_t>::operator-=( const Instance<real_t> &)
     {
+        length = std::max( length, other.length);
+        std::for_each( std::begin(other.vector), std::end(other.vector), [&vector](std::pair<size_t,real_t> p)
+                                                                            {
+                                                                                auto iterator = vector.find( p.first);
+                                                                                if( iterator != m.end())
+                                                                                    vector[p.first] -= p.second;
+                                                                                else
+                                                                                    vector[p.first] = 0 - p.second;
+                                                                            }
+                                                                        );
         return *this;
     }
-    Instance<real_t>& Instance<real_t>::operator*=( const Instance<real_t> &)
-    {
-        return *this;
-    }
-
 
     //--------------------------- Operators: +, -, * ----------------------------------------------------
-    template<real_t> Instance<real_t> operator+( Instance<real_t> lhs, const Instance<real_t> & rhs)
+    template<typename real_t> 
+    Instance<real_t> operator+( Instance<real_t> lhs, const Instance<real_t> & rhs)
     {
         lhs += rhs;
         return lhs;
     }
 
-    template<real_t> Instance<real_t> operator-( Instance<real_t> lhs, const Instance<real_t> & rhs)
+    template<typename real_t> 
+    Instance<real_t> operator-( Instance<real_t> lhs, const Instance<real_t> & rhs)
     {
         lhs -= rhs;
         return lhs;
     }
 
-    template<real_t> Instance<real_t> operator*( Instance<real_t> lhs, const Instance<real_t> & rhs)
+    template<typename real_t>
+    real_t operator*( Instance<real_t> lhs, const Instance<real_t> & rhs)
     {
-        lhs *= rhs;
-        return lhs;
+        std::vector<real_t> mapped;
+        std::transform( std::begin(lhs.vector), std::end(lhs.vector), std::begin(mapped),
+                        [&lhs.vector,rhs.vector](std::pair<size_t,real_t> p)
+                            {
+                                auto iterator = (rhs.vector).find( p.first);
+                                if( iterator != m.end())
+                                    return lhs.vector[p.first] * iterator->second;
+                                else
+                                    return .0;
+                            }
+                    );
+        return std::accumulate( std::begin(mapped), std::end(mapped), .0);
     }
 
-    template<real_t> bool operator==( Instance<real_t> lhs, const Instance<real_t> & rhs)
+    template<typename real_t>
+    Instance<real_t> operator*( real_t scalar,const Instance<real_t> & instance)
     {
-        //------------------------------------------------------------------------------------------------
-        return false;
+        std::map<size_t,real_t> result;
+        if(scalar)
+            std::for_each(std::begin(instance.vector), std::end(instance.vector),
+                            [scalar,&result](std::pair<size_t,real_t> p)
+                            {
+                                result[p.first] *= scalar;
+                            } 
+                        );
+        return ;
     }
 
-    template<real_t> bool operator!=( Instance<real_t>lhs, const Instance<real_t> & rhs)
+    //--------------------------- Operators: ==, != ----------------------------------------------------
+    template<typename real_t>
+    bool operator==( Instance<real_t> lhs, const Instance<real_t> & rhs)
+    {
+        return std::all_of(std::begin( rhs.vector), std::end( rhs.vector), 
+                            [=](std::pair<size_t,real_t> p)
+                            {
+                                auto iterator = (lhs.vector).find(p.first);
+                                if(iterator != end)
+                                    return std::abs( lhs.vector[p.first] - rhs[p.first]) < std::numeric_limits<real_t>::epsilon();
+                                else
+                                    return std::abs( rhs.vector[p.first]) < std::numeric_limits<real_t>::epsilon();
+                            }
+                        );
+    }
+
+    template<typename real_t>
+    bool operator!=( Instance<real_t>lhs, const Instance<real_t> & rhs)
     {
         return !operator==(lhs,rhs);
     }
 
-    std::ostream & Instance<real_t>::operator<<( std::ostream &, const Instance<real_t> &)
+    //----------------------------- Operators: << --------------------------------------------------------
+    template<typename real_t>
+    std::ostream & operator<<( std::ostream & os, const Instance<real_t> & instance)
     {
-
+        os << <<;
+        return os;
     }
 
 }
--- src/Instance/instance.h
+++ src/Instance/instance.h
@@ -1,9 +1,16 @@
 #pragma once
 
 #include <iostream>
+
 #include <map>
+
 #include <utility>
+#include <numeric>
 #include <algorithm>
+
+#include <limits>
+
+#include <cmath>
 
 namespace lasvm
 {
@@ -14,17 +21,17 @@ namespace lasvm
         Instance( void);
         Instance( std::istream &);
         Instance( const Instance<real_t> &);
-        Instance( const Instance<real_t> &&);
+        Instance( Instance<real_t> && );
         ~Instance( void);
 
         void swap( const Instance<real_t> & );
-        Instance<real_t>& operator=( Instance<real_t>);
+        Instance<real_t>& operator=( Instance<real_t> );
+        Instance<real_t>& operator=( Instance<real_t> && );
 
         real_t& operator[](size_t );
 
         Instance& operator+=( const Instance<real_t> &);
         Instance& operator-=( const Instance<real_t> &);
-        Instance& operator*=( const Instance<real_t> &);
 
     private:
         std::map<size_t,real_t> vector;
