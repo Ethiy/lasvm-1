8de812e01f1febc47c4636a0bd72f777e599b6c9
ETHIY <oussama.ennafii@polytechnique.org>
2017-02-19 15:50:39 +0100

    new sketch

--- src/Instance/instance.h
+++ src/Instance/instance.h
@@ -14,6 +14,19 @@
 
 namespace lasvm
 {
-    template <typename real_type>
-    class Instance {};
+    template <typename real_t>
+    class Instance
+    {
+    public:
+        Instance(void);
+        Instance(std::size_t _size, real_t* _vector);
+        Instance(std::map<size_t, real_t> sparse_vector);
+        ~Instance(void);
+
+        template<typename real_t>
+        void swap(Instance<real_t> & other);
+    private:
+        std::size_t size;
+        std::unique_ptr<real_t> vector;
+    };
 }
